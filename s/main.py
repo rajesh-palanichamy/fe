325ebd3142c35a7e83e1d1cd29ec74e6324110eb
Rajesh <rajesh.palanichamy@gmail.com>
2020-02-10 01:08:29 +0530

    Add Hello World app to Cloud Source Repositories

---
+++ s/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello,Rajesh and  World!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
+
